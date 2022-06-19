690ba5572f737790de14debb9070a20a5e0ca6cc
Yevhen Volontyr <soapforwork@gmail.com>
2022-06-19 15:16:05 +0300

    first commit

---
+++ precache-manifest.805775d98650866b92fd1fd008c50ed6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24d9b9392c53ccf524179d925e28aab7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec0d30f084c5639b21fe",
+    "url": "/static/css/main.2dd63d0e.chunk.css"
+  },
+  {
+    "revision": "69ee5889b551dd6c71d7",
+    "url": "/static/js/2.5cb2b1d1.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.5cb2b1d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec0d30f084c5639b21fe",
+    "url": "/static/js/main.0b46e33b.chunk.js"
+  },
+  {
+    "revision": "4af1f40934cfee098524",
+    "url": "/static/js/runtime-main.12e8f38e.js"
+  }
+]);
